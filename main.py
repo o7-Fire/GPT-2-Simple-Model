e81c1b7c0a33adc0158427bd21e3fc6b9ef28cf7
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-03-23 03:14:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import gpt_2_simple as gpt2
 import os.path
 import gdown
 if not os.path.isfile("run2.tar"):
-  url = 'https://drive.google.com/u/0/uc?id=1T5BntQPxoRjGDMAUbqUwUnsDs2DARM6C&export=download'
+  url = 'https://drive.google.com/u/0/uc?id=1T5BntQPxoRjGDMAUbqUwUnsDs2DARM6C&export=download'#You can set your to yoour own model on gdrive
   output = 'run2.tar'
   print("Downloading pretrained model...")
   gdown.download(url, output, quiet=False)
